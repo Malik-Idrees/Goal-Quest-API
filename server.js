d900b61b18600c8c5ea5383f23b3ea1db5ac3bff
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-11-04 18:59:20 +0500

    fix(heroku): Made the port dynamic-No binding issues

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
-    5000,
+    PORT,
     console.log(
         `Server running on ${PORT} in ${process.env.NODE_ENV}`.yellow.bold
     )
